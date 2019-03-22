ef86467cd6056a86b6fd629ff100780fa62fa003
Marcus++ <jeannet.marc@gmail.com>
2019-03-22 12:51:02 +0100

    cleanup + TabNav options for futur uses

--- gbr/src/routes/TabNav.js
+++ gbr/src/routes/TabNav.js
@@ -1,23 +1,25 @@
 import * as React from "react"
-import { Platform } from "react-native"
+import { Platform, Text } from "react-native"
 import { createStackNavigator, createBottomTabNavigator } from "react-navigation"
 /**/
 import HomePage from "../components/HomePage"
 import BattlePage from "../components/BattlePage"
 import PopularPage from "../components/PopularPage"
 
-// const homeStack = createStackNavigator({
-//     Home, HomePage
-// })
-// homeStack.navigationOptions = {
-//     tabBarLabel: "Home",
-//     tabBarIcon: ({focused}) => ( <TabBarIcon focused={focused} /> )
-// }
 
 const TabNav = createBottomTabNavigator({
-    Home: HomePage,
-    Battle: BattlePage,
-    Popular: PopularPage
+    Home: {
+        screen: HomePage,
+        navigationOptions: { tabBarIcon: ({}) => (<Text>Home</Text>) }
+    },
+    Battle: {
+        screen: BattlePage,
+        navigationOptions: { tabBarIcon: ({}) => (<Text>Battle</Text>) }
+    },
+    Popular: {
+        screen: PopularPage,
+        navigationOptions: { tabBarIcon: ({}) => (<Text>Popular</Text>) }
+    },
   });
 
   export default TabNav
