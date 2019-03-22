bc1f7d07edd2f0d709843ee4f75094168c9c2ddc
Marcus++ <jeannet.marc@gmail.com>
2019-03-22 15:41:32 +0100

    form with GET request in console.log()

--- gbr/src/components/BattlePage.js
+++ gbr/src/components/BattlePage.js
@@ -1,12 +1,17 @@
 import * as React from "react"
 import {View, Text} from "react-native"
+/**/
+import SelectUserForm from "./_selectUserForm"
 
 export default class BattleScreen extends React.Component {
     static navigationOptions = {header: null}
 
     render() {
         return(
-            <View><Text>Battle Page</Text></View>
+            <View>
+                <Text>Battle Page</Text>
+                <SelectUserForm />
+                </View>
         )
     }
 }
